a7a16ec96bee22de0e4df918dd023ef7bdc950c7
Oluwadare Titus olamide <horlly582@gmail.com>
2026-03-12 22:56:51 +0100

    nm

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,23 +7,22 @@ function Contact() {
         <div className="bg-[#B7950B] border rounded-t-2xl border-amber-50 flex justify-center py-3 sm:py-5 font-bold text-base sm:text-xl text-white">
           GET 10% OFF
         </div>
-        <div className="md:hidden px-4 py-4 bg-transparent text-white">
-          <div className="flex items-center justify-between relative">
-            <div></div>
-            <img src={logo} alt="Logo" className="h-12 w-auto" />
-            <div className="flex items-center gap-3 font-semibold">
-              <span className="text-xs">Search</span>
-              <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.16.12-.33.12-.5 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
-              </svg>
-              <span className="text-xs">0</span>
-            </div>
-          </div>
-          <div className="mt-4 flex items-center justify-start gap-5 text-sm font-semibold">
+        <div className="md:hidden px-3 py-3 bg-transparent text-white relative flex items-center justify-between">
+          <div className="flex gap-3 font-semibold text-xs">
             <nav>Home</nav>
             <nav>Shop</nav>
             <nav>News</nav>
             <nav>Contact</nav>
+          </div>
+          <div className="absolute left-1/2 -translate-x-1/2">
+            <img src={logo} alt="Logo" className="h-10 w-auto" />
+          </div>
+          <div className="flex items-center gap-2 font-semibold text-xs">
+            <span>Search</span>
+            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.16.12-.33.12-.5 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
+            </svg>
+            <span>0</span>
           </div>
         </div>
 
